c9ed2419c56fe78f3f55f46beaa3fb7b4397f9b5
Alexander Komla <kunalexander360@gmail.com>
2025-03-10 11:39:42 +0000

    Create dataloaders and visualize images

--- generative_adversarial_networks.py
+++ generative_adversarial_networks.py
@@ -4,14 +4,16 @@ import torch.nn.functional as F
 from torch.utils.data import DataLoader 
 
 from torchvision.datasets import ImageFolder 
+from torchvision.utils import make_grid
 import torchvision.transforms as T 
+
+import matplotlib.pyplot as plt
 
 from pathlib import Path
 
 
 
 TRAIN_DIR = Path("data")
-print(type(TRAIN_DIR))
 IMAGE_SIZE = 64 
 BATCH_SIZE = 128 
 STATS = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
@@ -23,6 +25,26 @@ train_ds = ImageFolder(TRAIN_DIR, transform=T.Compose([
     T.Normalize(*STATS)
 ])) 
 
-print(train_ds[0])
+train_dl = DataLoader(train_ds, 
+                    shuffle=True,
+                    batch_size=BATCH_SIZE, 
+                    pin_memory=True) 
+
+def denorm(img_tensors):
+    return img_tensors * STATS[1][0] + STATS[0][0]  
+
+def show_images(images, nmax=64):
+    fig, ax = plt.subplots(figsize=(8, 8))
+    ax.set_xticks([]); ax.set_yticks([]) 
+    ax.imshow(make_grid( denorm(images.detach()[:nmax]), nrow=8).permute(1, 2, 0))
+
+def show_batch(dl, nmax=64): 
+    for images, _ in dl:
+        show_images(images, nmax)
+        break 
+
+show_batch(train_dl)
+plt.show()
+
 
 
